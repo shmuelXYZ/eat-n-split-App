934d9bbaf2a97df641171a278fc5d41a37e51ec4
shmulik <shmuel.harari1@gmail.com>
2024-10-08 17:00:34 +0300

    2 new components friend, list of friends,and clean files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <ListOfFriends />
+      </div>
     </div>
+  );
+};
+
+export default App;
+
+function Friend({ name, image, balance }) {
+  return (
+    <li>
+      <img src={image} alt=""></img>
+      <h3>{name}</h3>
+      {balance === 0 ? (
+        <p>you and {name} are even</p>
+      ) : balance < 0 ? (
+        <p className="red">
+          you owe to {name} {Math.abs(balance)}$
+        </p>
+      ) : (
+        <p className="green">
+          {name} owe you {balance}$
+        </p>
+      )}
+      <button className="button">Select</button>
+    </li>
   );
 }
 
-export default App;
+function ListOfFriends() {
+  return (
+    <ul>
+      {initialFriends.map((friend) => (
+        <Friend
+          key={friend.id}
+          image={friend.image}
+          name={friend.name}
+          balance={friend.balance}
+        />
+      ))}
+    </ul>
+  );
+}
